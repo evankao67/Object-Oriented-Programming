8b927b562f1063615bdddb53100162788fdc6461
evankao67 <71363010+evankao67@users.noreply.github.com>
2022-01-22 15:45:51 +0800

    Create buffer.cpp

---
+++ SelfPractice/buffer.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+int main(){
+   char buffer1[100], evan, buffer2[100];
+   cin>>buffer1>>evan>>buffer2;
+   cout<<buffer1<<evan<<buffer2;
+}
