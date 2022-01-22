11b7c10c5dd23cd1f2b7ee353005a9b91016f2dc
evankao67 <71363010+evankao67@users.noreply.github.com>
2022-01-22 15:46:00 +0800

    Create definenanipulator.cpp

---
+++ SelfPractice/definenanipulator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define ln '\n'
+ostream &ta(ostream &s)
+{
+    cout<<"ajhwhd"<<'\t';
+    return s<<ln;
+}
+int main()
+{
+    char x='A';
+    cout<<ta<<x<<ln;
+}
