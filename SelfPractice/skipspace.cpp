556b6bca0e961a389fbe799213e3352092465d54
evankao67 <71363010+evankao67@users.noreply.github.com>
2022-01-22 15:46:12 +0800

    Create skipspace.cpp

---
+++ SelfPractice/skipspace.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define ln '\n'
+int main()
+{
+    char x;
+    cin.unsetf(ios::skipws);//when skip white space, system catch the space before input
+    cin>>x;
+    cout<<x<<ln;
+    cin.setf(ios::skipws);
+}
